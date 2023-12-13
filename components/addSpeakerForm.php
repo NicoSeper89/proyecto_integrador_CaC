0f7b303532b2d178af11238e7536e3f789044736
nicoseper89 <nicosepertino@hotmail.com>
2023-12-13 10:41:56 -0300

    fix validations to add speaker form

--- components/addSpeakerForm.php
+++ components/addSpeakerForm.php
@@ -1,25 +1,25 @@
-<form class="row g-3" action="handlers/add-speaker-form-handler.php" method="post" id="form">
+<form class="row g-3 form" action="handlers/add-speaker-form-handler.php" method="post" id="form1">
     <div class="col-6">
-        <input type="text" class="inputText form-control" id="inputName" name="name" maxlength="30" placeholder="Nombre" />
+        <input type="text" class="inputText1 form-control" id="inputName" name="name" maxlength="30" placeholder="Nombre" autocomplete="off"/>
         <div id="inputName" class="invalid-feedback">
             Ingrese su nombre
         </div>
     </div>
     <div class="col-6">
-        <input type="text" class="inputText form-control" id="inputSurname" name="surname" maxlength="30" placeholder="Apellido" />
+        <input type="text" class="inputText1 form-control" id="inputSurname" name="surname" maxlength="30" placeholder="Apellido" autocomplete="off"/>
         <div id="inputSurname" class="invalid-feedback">
             Ingrese su apellido
         </div>
     </div>
     <div class="col-12">
-        <input type="email" class="inputText form-control" id="inputEmail" name="email" maxlength="30" placeholder="Email" />
+        <input type="email" class="inputText1 form-control" id="inputEmail" name="email" maxlength="30" placeholder="Email" autocomplete="off"/>
         <div id="inputEmail" class="invalid-feedback">
             Ingrese su email
         </div>
     </div>
     <div class="col-12">
-        <textarea type="text" class="inputText form-control" id="inputSubject" name="subject" maxlength="60" placeholder="Sobre qué quieres hablar?"></textarea> 
-        <div id="inputEmail" class="invalid-feedback">
+        <textarea type="text" class="inputText1 form-control" id="inputSubject" name="subject" maxlength="60" placeholder="Sobre qué quieres hablar?" autocomplete="off" ></textarea> 
+        <div id="inputSubject" class="invalid-feedback">
             Ingrese un título para su charla
         </div>
         <p class="fw-lighter text-muted mb-2">
